77ae671a3f22fd5319111c05befa7a7235c16eed
aryancodes1 <aryankaul539@gmail.com>
2024-07-03 14:29:34 +0530

    Added

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -1,9 +1,47 @@
 import streamlit as st
 import ollama
-from data_loading import collection,load_data
 import os
 import tempfile
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+import ollama
+from langchain_community.document_loaders import PyMuPDFLoader
 
+client = chromadb.Client()
+
+try:
+    collection = client.create_collection(name="docs")
+except chromadb.db.base.UniqueConstraintError:
+    collection = client.get_collection(name="docs")
+
+
+def load_data(path, n):
+    loader = PyMuPDFLoader(path)
+
+    data = loader.load()
+
+    list1 = []
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    text_splitter.split_documents(data)
+
+    for i in range(len(data)):
+        data_text = data[i].page_content
+        data_text = data_text.replace("\n", "")
+        list1.append(data_text)
+
+    ##Storing Embedded Data
+
+    for i, data in enumerate(list1):
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=data)
+        embedding = response["embedding"]
+        collection.add(ids=[str(i) + str(n)], embeddings=[embedding], documents=[data])
+    print("Done")
 
 def get_bot_response(user_input):
 
@@ -32,8 +70,11 @@ def main():
         path = os.path.join(temp_dir, uploaded_file.name)
         with open(path, "wb") as f:
                 f.write(uploaded_file.getvalue())
-
-    load_data(path,'a')
+        try:
+            load_data(path,'a')
+        except UnboundLocalError:
+            st.write("Loading")
+        
 
     user_input = st.text_input("Enter your message:")
 
